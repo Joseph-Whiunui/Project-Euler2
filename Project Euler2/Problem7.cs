d5f433b2db22ef53eb7c0a76e4c6f6720a3d2b1f
Joseph-Whiunui <jwhiunui@gmail.com>
2018-01-31 15:36:00 +1300

    Solved problem 7 and updated class Solver to have Selector method

---
+++ Project Euler2/Problem7.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+//What is the 10 001st prime number?
+
+namespace Project_Euler2
+{
+    class Problem7
+    {
+        public static int Workout()
+        {
+            int primeSize = 0;
+            int currNum = 2;
+            HashSet<int> primes = new HashSet<int>();
+            bool isPrime = true;
+            int currPrime = 2;
+
+            while (primeSize < 10001)
+            {
+                
+                isPrime = true;
+                foreach (int prime in primes)
+                {
+                    if (currNum % prime == 0)
+                    {
+                        isPrime = false;
+                        break;                     
+                    }
+                }
+
+                if (isPrime)
+                {
+                    primes.Add(currNum);
+                    primeSize++;
+                }
+
+                currPrime = currNum;
+                currNum++;
+
+            }
+
+            return currPrime;
+            
+        }
+        
+    }
+}
---
+++ Project Euler2/Solver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler2
+{
+    class Solver
+    {
+        static void Main(string[] args)
+        {
+            int probNum = 0;
+            Console.WriteLine("Enter a problem number from 7-7");
+            string input = Console.ReadLine();
+            while (!Int32.TryParse(input, out probNum) || probNum < 7 || probNum > 7)
+            {
+                Console.WriteLine("Invalid input. Please enter a problem number in range 7-7");
+                input = Console.ReadLine();
+            }
+            Selector(probNum);
+            Console.WriteLine("finished");
+            Console.ReadLine();
+        }
+
+        // selects the problem the user wants to solve
+        static void Selector(int probNum)
+        {
+            switch(probNum)
+            {
+                case 7:
+                    Console.WriteLine(Problem7.Workout());
+                    break;
+                default:
+                    Console.WriteLine("Problem not solved");
+                    break;
+            }
+        }
+    }
+}
